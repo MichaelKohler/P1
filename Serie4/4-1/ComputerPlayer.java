81d7391934b0d9bd8e8d9c1e9628425891b64fb7
Lukas Diener <lukas.diener@hotmail.com>
2011-11-24 14:35:09 +0100

    ComputerPlayer ready

--- Serie4/4-1/ComputerPlayer.java
+++ Serie4/4-1/ComputerPlayer.java
@@ -1,52 +1,204 @@
 /*
- Programmierung 1 HS 2011
- Aufgabe 4-1
- 
- Michael Kohler - 11-108-289
- Lukas Diener - 11-123-213
-*/
+Programmierung 1 HS 2011
+Aufgabe 4-1
+
+Michael Kohler - 11-108-289
+Lukas Diener - 11-123-213
+ */
 
 /** A very stupid computer player */
-public class ComputerPlayer implements IPlayer{
-	private VierGewinnt.Token token;
-	
-	public int getNextColumn(VierGewinnt.Token[][] board){
-		java.util.Random generator = new java.util.Random();
-		boolean columnOK = false;
-		int col = 0;
-		while(!columnOK){
-			// random number between 0 and 6
-			col = generator.nextInt(board.length);
-			// check if this column is still "free"
-			if(isColFull(col, board) == false) columnOK = true;
-			// if not, we have to generate another random number
-			else columnOK = false;
-		}
-		return col;
-	}
-	
-	
-	
-	/** returns true if the column col is already full and false otherwise. */
-	private boolean isColFull(int col, VierGewinnt.Token[][] board){
-		int topRow = board[0].length-1;
-		if(board[col][topRow] != VierGewinnt.Token.empty){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	
-	public void setToken(VierGewinnt.Token token){
-		this.token = token;
-	}
-	
-	public VierGewinnt.Token getToken(){
-		return this.token;
-	}
-	
-	public String getProgrammers(){
-		return "Lukas Diener, Michael Kohler";
-	}
+public class ComputerPlayer implements IPlayer {
+
+    public static boolean aboutToWin;
+    private VierGewinnt.Token token;
+    public static final int maxScore = 100;			//max score for field
+    public static final int invalidScore = 1000000;         //invalid score
+    private static boolean useEvaluationFunction = true;
+    private static int maxDepth = 8;
+    public static int[] moveScore = new int[VierGewinnt.COLS];
+
+    public static void main(String args[]) {
+        for (int x = 0; x < VierGewinnt.COLS; ++x) {
+            moveScore[x] = 0;
+        }
+    }
+
+    public int getNextColumn(VierGewinnt.Token[][] board) {
+        /*java.util.Random generator = new java.util.Random();
+        boolean columnOK = false;
+        int col = 0;
+        while(!columnOK){
+        // random number between 0 and 6
+        col = generator.nextInt(board.length);
+        // check if this column is still "free"
+        if(isColFull(col, board) == false) columnOK = true;
+        // if not, we have to generate another random number
+        else columnOK = false;
+        }
+        return col;*/
+        return determineMove('X');
+
+    }
+
+    /** returns true if the column col is already full and false otherwise. */
+    private boolean isColFull(int col, VierGewinnt.Token[][] board) {
+        int topRow = board[0].length - 1;
+        if (board[col][topRow] != VierGewinnt.Token.empty) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setToken(VierGewinnt.Token token) {
+        this.token = token;
+    }
+
+    public VierGewinnt.Token getToken() {
+        return this.token;
+    }
+
+    public String getProgrammers() {
+        return "Lukas Diener, Michael Kohler";
+    }
+
+    private static int alphabeta(char player) {
+        return alphabeta(player, -invalidScore, invalidScore, 0);
+    }
+
+    private static int alphabeta(char player, int alpha, int beta, int depth) {
+        int width = VierGewinnt.COLS;
+        int height = VierGewinnt.ROWS;
+        //Check if there's a current winner
+        char winner = VierGewinnt.checkWin(VierGewinnt.lastX, VierGewinnt.lastY);
+        if (winner == 'X') {
+            return maxScore;
+        } else if (winner == 'O') {
+            return -maxScore;
+        }
+
+        if (depth >= maxDepth) {
+            //We cannot recurse more, and we haven't found
+            //an end to the game
+            //Return the score of the current table
+            if (useEvaluationFunction) {
+                return VierGewinnt.getScore();
+            } else {
+                return 0;
+            }
+        }
+
+        if (player == 'X') {
+            //It is player X's turn, he will try to
+            //maximize the score
+
+            //Go through all possible moves and get the
+            //scores
+            int maxScore = -invalidScore;
+            for (int move = 0; move < width; ++move) {
+                if (VierGewinnt.canMove(move)) {
+                    VierGewinnt.move(move, 'X');
+                    int score = alphabeta('O', alpha, beta, depth + 1);
+                    VierGewinnt.undo(move, 'X');
+                    if (score > alpha) {
+                        alpha = score;
+                    }
+                    if (alpha >= beta) {
+                        return alpha;
+                    }
+                }
+            }
+            return alpha;
+        } else if (player == 'O') {
+            //It is player X's turn, he will try to
+            //maximize the score
+
+            //Go through all possible moves and get the
+            //scores
+            int minScore = invalidScore;
+            for (int move = 0; move < VierGewinnt.COLS; ++move) {
+                if (VierGewinnt.canMove(move)) {
+                    VierGewinnt.move(move, 'O');
+                    int score = alphabeta('X', alpha, beta, depth + 1);
+                    VierGewinnt.undo(move, 'O');
+                    if (score < beta) {
+                        beta = score;
+                    }
+                    if (alpha >= beta) {
+                        return beta;
+                    }
+                }
+            }
+            return beta;
+        } else //This is just for safty if the function
+        //is called with wron parameters
+        {
+            return 0;
+        }
+    }
+
+    private static int determineMove(char player) {
+
+        if (player == 'X') {
+            //It is player X's turn, he will try to
+            //maximize the score
+
+            //Go through all possible moves and get the
+            //scores
+            int maxScore = -invalidScore;
+            int maxMove = 0;
+            for (int move = 0; move < VierGewinnt.COLS; ++move) {
+                if (VierGewinnt.canMove(move)) {
+                    VierGewinnt.move(move, 'X');
+                    int score = alphabeta('O');
+                    ComputerPlayer.moveScore[move] = score;
+
+
+                    if (score >= maxScore) {
+                        maxScore = score;
+                        maxMove = move;
+                    }
+                    VierGewinnt.undo(move, 'X');
+                } else //Just set the move score to an invalid score
+                {
+                    moveScore[move] = invalidScore;
+                }
+            }
+
+            //Return the move with the highest score
+            return maxMove;
+            
+        } else if (player == 'O') {
+            //It is player O's turn, he will try to
+            //minimize the score
+
+            //Go through all possible moves and get the
+            //scores
+            int minScore = invalidScore;
+            int minMove = 0;
+            for (int move = 0; move < VierGewinnt.COLS; ++move) {
+                if (VierGewinnt.canMove(move)) {
+                    VierGewinnt.move(move, 'O');
+                    int score = alphabeta('X');
+
+                    moveScore[move] = score;
+
+                    if (score < minScore) {
+                        minScore = score;
+                        minMove = move;
+                    }
+                    VierGewinnt.undo(move, 'O');
+                } else //Just set the move score to an invalid score
+                {
+                    moveScore[move] = invalidScore;
+                }
+            }
+            //Return the move with the least score
+            return minMove;
+        } else //This is just for safty if the function
+        //is called with wrong parameters
+        {
+            return 0;
+        }
+    }
 }
--- Serie4/4-1/VierGewinnt.java
+++ Serie4/4-1/VierGewinnt.java
@@ -1,226 +1,417 @@
 /*
- Programmierung 1 HS 2011
- Aufgabe 4-1
- 
- Michael Kohler - 11-108-289
- Lukas Diener - 11-123-213
-*/
+Programmierung 1 HS 2011
+Aufgabe 4-1
+
+Michael Kohler - 11-108-289
+Lukas Diener - 11-123-213
+ */
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class VierGewinnt {
-	
-	public static final int COLS = 7;
-	public static final int ROWS = 6;
-	
-	// Enum for the different tokens (empty, O, X)
-	public enum Token { 
-		empty(" "),
-		player1("O"),
-		player2("X");
-		private String representation; // string representation of value
-		Token(String s) { this.representation = s; }
-		public String toString() { return this.representation; }
-	}
-	
-	private Token[][] board = new Token[COLS][ROWS]; // 7 columns with 6 fields each
-	private IPlayer[] players = new IPlayer[2]; // two players
-	
-	private Token correctToken;
-	
-	/** initialize board and players and start the game */
-	public void play() {
-		// initialize the board
-		for (Token[] column : this.board) {
-			Arrays.fill(column, Token.empty);
-		}
-		// initialize players
-		players[0] = new HumanPlayer();
-		System.out.print("Play against a human opponent? (y / n) ");
-		String opponent = new Scanner(System.in).nextLine();
-		if(opponent.toLowerCase().equals("y")){
-			players[1] = new HumanPlayer();
-		}else{
-			players[1] = new ComputerPlayer();
-		}
-		players[0].setToken(Token.player1);
-		players[1].setToken(Token.player2);
-		// play...
-		boolean solved = false;
-		int currentPlayer = (new java.util.Random()).nextInt(2);  //choose randomly who begins
-		System.out.println("current player: "+currentPlayer);
-		int insertCol, insertRow; // starting from 0
-		while (!solved && !this.isBoardFull()){
-			// get player's next "move"
-			insertCol = players[currentPlayer].getNextColumn(this.board);
-			// insert the token and get the row where it landed
-			insertRow = this.insertToken(insertCol, players[currentPlayer].getToken());
-			// check if the game is over
-			solved = this.checkVierGewinnt(insertCol, insertRow);
-			//switch to other player
-			if (!solved) currentPlayer = (currentPlayer+1) % 2;
-		}
-		System.out.println(displayBoard(this.board));
-		if (solved) System.out.println("Player "+players[currentPlayer].getToken() + " wins!");
-		else System.out.println("Draw! Game over.");
-	}
-	
-	
-	/** Inserts the token at the specified column (if possible)
-	    and returns the row where the token landed */
-	    
-	/** !!!!! Achtung: in der Aufgabenstellung steht, dass das Programm bei ungültiger
-	    Eingabe mit Fehlermeldung terminieren soll. Hier ist das aber gar nicht
-	    noetig, da eine ungültige Spaltenangabe abgefangen wird und neu gefragt
-	    wird. !!!!! */
-	private int insertToken(int column, Token tok){
-		int row = -1;
-		for (int i = 0; i < board[column].length; i++) {
-		  if (board[column][i].equals(Token.empty)) {
-		    row = i;
-		    break;
-		  }
-		}
-		board[column][row] = tok;
-		return row;
-	}
-	
-	
-	/** Checks if every position is occupied */
-	private boolean isBoardFull() {
-		boolean full = true;
-		for (int i = 0; i < this.COLS; i++) {
-		  if (board[i][this.ROWS - 1].equals(Token.empty))
-		    full = false;
-		}
-		return full;
-	}
-	
-	
-	/** Checks for at least four equal tokens in a row in either direction,
-	    starting from the given position. */
-	private boolean checkVierGewinnt(int col, int row){
-	  this.correctToken = board[col][row];
-	
-		boolean horizontal = checkHorizontal(col, row);
-		boolean vertical = checkVertical(col, row);
-		boolean diagonal1 = checkDiagonal1(col, row);
-		boolean diagonal2 = checkDiagonal2(col, row);
 
-    return (horizontal || vertical || diagonal1 || diagonal2);
-	}
+    public static final int COLS = 7;
+    public static final int ROWS = 6;
+    private static int toWin = 4;      //4 gewinnt
+    public static int lastX = 0;
+    public static int lastY = 0;
 
-  private boolean checkHorizontal(int col, int row) {
-    int counter = 1;
-    for (int i = col + 1; i < this.COLS; i++) {
-      if (board[i][row].equals(this.correctToken))
-        counter++;
-      else
-        break;
+    // Enum for the different tokens (empty, O, X)
+    public enum Token {
+
+        empty(" "),
+        player1("O"),
+        player2("X");
+        private String representation; // string representation of value
+
+        Token(String s) {
+            this.representation = s;
+        }
+
+        public String toString() {
+            return this.representation;
+        }
     }
-    
-    for (int i = col - 1; i >= 0; i--) {
-      if (board[i][row].equals(this.correctToken))
-        counter++;
-      else
-        break;
+    private static Token[][] board = new Token[COLS][ROWS]; // 7 columns with 6 fields each
+    private static IPlayer[] players = new IPlayer[2]; // two players
+    private Token correctToken;
+
+    /** initialize board and players and start the game */
+    public void play() {
+        // initialize the board
+        for (Token[] column : VierGewinnt.board) {
+            Arrays.fill(column, Token.empty);
+        }
+
+
+        // initialize players
+        players[0] = new HumanPlayer();
+        System.out.print("Play against a human opponent? (y / n) ");
+        String opponent = new Scanner(System.in).nextLine();
+        if (opponent.toLowerCase().equals("y")) {
+            players[1] = new HumanPlayer();
+        } else {
+            players[1] = new ComputerPlayer();
+        }
+        players[0].setToken(Token.player1);
+        players[1].setToken(Token.player2);
+        // play...
+        boolean solved = false;
+        int currentPlayer = (new java.util.Random()).nextInt(2);  //choose randomly who begins
+        System.out.println("current player: " + currentPlayer);
+        int insertCol, insertRow; // starting from 0
+        while (true) {
+            // get player's next "move"
+            lastX = insertCol = players[currentPlayer].getNextColumn(VierGewinnt.board);
+            // insert the token and get the row where it landed
+            lastY = insertRow = this.insertToken(insertCol, players[currentPlayer].getToken());
+            // check if the game is over
+            if(VierGewinnt.checkWin(insertCol, insertRow) != ' ' || isBoardFull())
+                break;
+            
+            //char bla = VierGewinnt.checkWin(insertCol, insertRow);
+            //switch to other player
+            currentPlayer = (currentPlayer + 1) % 2;
+        }
+        char winner = VierGewinnt.checkWin(lastX, lastY);
+        System.out.println(displayBoard(VierGewinnt.board));
+        if (winner != ' ') {
+            System.out.println("Player " + winner + " wins!");
+        } else{
+            System.out.println("Draw! Game over.");
+        }
     }
-    
-    return (counter == 4);
-  }
-	
-	private boolean checkVertical(int col, int row) {
-    int counter = 1;
-    for (int i = row + 1; i < this.ROWS; i++) {
-      if (board[col][i].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    /** Inserts the token at the specified column (if possible)
+    and returns the row where the token landed */
+    /** !!!!! Achtung: in der Aufgabenstellung steht, dass das Programm bei ungültiger
+    Eingabe mit Fehlermeldung terminieren soll. Hier ist das aber gar nicht
+    noetig, da eine ungültige Spaltenangabe abgefangen wird und neu gefragt
+    wird. !!!!! */
+    private int insertToken(int column, Token tok) {
+        int row = -1;
+        for (int i = 0; i < board[column].length; i++) {
+            if (board[column][i].equals(Token.empty)) {
+                row = i;
+                break;
+            }
+        }
+        board[column][row] = tok;
+        return row;
     }
-    
-    for (int i = row - 1; i >= 0; i--) {
-      if (board[col][i].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    /** Checks if every position is occupied */
+    private boolean isBoardFull() {
+        boolean full = true;
+        for (int i = 0; i < COLS; i++) {
+            if (board[i][ROWS - 1].equals(Token.empty)) {
+                full = false;
+            }
+        }
+        return full;
     }
-    
-    return (counter == 4);
-  }
-  
-  private boolean checkDiagonal1(int col, int row) {
-    int counter = 1;
-    int i, j;
-    for (i = row + 1, j = col + 1; i < this.ROWS && j < this.COLS;
-         i++, j++) {
-      if (board[j][i].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    /** Checks for at least four equal tokens in a row in either direction,
+    starting from the given position. */
+    private boolean checkVierGewinnt(int col, int row) {
+        this.correctToken = board[col][row];
+
+        boolean horizontal = checkHorizontal(col, row);
+        boolean vertical = checkVertical(col, row);
+        boolean diagonal1 = checkDiagonal1(col, row);
+        boolean diagonal2 = checkDiagonal2(col, row);
+
+        return (horizontal || vertical || diagonal1 || diagonal2);
     }
-    
-    int k, l;
-    for (k = row - 1, l = col - 1; k >= 0 && l >= 0; k--, l--) {
-      if (board[l][k].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    private boolean checkHorizontal(int col, int row) {
+        int counter = 1;
+        for (int i = col + 1; i < VierGewinnt.COLS; i++) {
+            if (board[i][row].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = col - 1; i >= 0; i--) {
+            if (board[i][row].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        return (counter == 4);
     }
-    
-    return (counter == 4);
-  }
-  
-  private boolean checkDiagonal2(int col, int row) {
-    int counter = 1;
-    int i, j;
-    for (i = row + 1, j = col - 1; i < this.ROWS && j >= 0;
-         i++, j--) {
-      if (board[j][i].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    private boolean checkVertical(int col, int row) {
+        int counter = 1;
+        for (int i = row + 1; i < VierGewinnt.ROWS; i++) {
+            if (board[col][i].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[col][i].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        return (counter == 4);
     }
-    
-    int k, l;
-    for (k = row - 1, l = col + 1; k >= 0 && l < this.COLS; k--, l++) {
-      if (board[l][k].equals(this.correctToken))
-        counter++;
-      else
-        break;
+
+    private boolean checkDiagonal1(int col, int row) {
+        int counter = 1;
+        int i, j;
+        for (i = row + 1, j = col + 1; i < VierGewinnt.ROWS && j < VierGewinnt.COLS;
+                i++, j++) {
+            if (board[j][i].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        int k, l;
+        for (k = row - 1, l = col - 1; k >= 0 && l >= 0; k--, l--) {
+            if (board[l][k].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        return (counter == 4);
     }
-    
-    return (counter == 4);
-  }
-	
-	/** returns a graphical representation of the board */
-	public static String displayBoard(Token[][] myBoard){
-		String rowDelimiter = "+";
-		String rowNumbering = " ";
-		for(int col=0; col<myBoard.length; col++){
-			rowDelimiter += "---+";
-			rowNumbering += " "+(col+1)+"  ";
-		}
-		rowDelimiter += "\n";
-		
-		String rowStr;
-		String presentation = rowDelimiter;
-		for(int row=myBoard[0].length-1; row >= 0; row--){
-			rowStr = "| ";
-			for(int col=0; col < myBoard.length; col++){
-				rowStr += myBoard[col][row].toString() + " | ";
-			}
-			presentation += rowStr + "\n" + rowDelimiter;
-		}
-		presentation += rowNumbering;
-		return presentation;
-	}
-	
-	
-	
-	/** main method, starts the program */
-	public static void main(String args[]){
-		VierGewinnt game = new VierGewinnt();
-		game.play();
-	}
+
+    private boolean checkDiagonal2(int col, int row) {
+        int counter = 1;
+        int i, j;
+        for (i = row + 1, j = col - 1; i < VierGewinnt.ROWS && j >= 0;
+                i++, j--) {
+            if (board[j][i].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        int k, l;
+        for (k = row - 1, l = col + 1; k >= 0 && l < VierGewinnt.COLS; k--, l++) {
+            if (board[l][k].equals(this.correctToken)) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        return (counter == 4);
+    }
+
+    public static char checkWin(int lastX, int lastY) {
+        //We only have to check around the last placed
+        //piece.
+        int x1, y1, x2, y2;
+        int width = COLS;
+        int height = ROWS;
+        char player = at(lastX, lastY);
+        //
+        //Test if there's a horizontal win
+        //
+        x1 = x2 = lastX;
+        //Go right
+        while (x1 < width && at(x1, lastY) == player) {
+            ++x1;
+        }
+        //Go left
+        while (x2 >= 0 && at(x2, lastY) == player) {
+            --x2;
+        }
+        //Do we have a win here?
+        if (x1 - x2 > toWin) {
+            return player;
+        }
+
+        //
+        //Test if there's a vertical win
+        //
+        y1 = y2 = lastY;
+        //Go up
+        while (y1 < height && at(lastX, y1) == player) {
+            ++y1;
+        }
+        //Go down
+        while (y2 >= 0 && at(lastX, y2) == player) {
+            --y2;
+        }
+        //Do we have a win here?
+        if (y1 - y2 > toWin) {
+            return player;
+        }
+
+        //                              X
+        // Do we have a win like this?    X
+        //                                  X
+        x1 = x2 = lastX;
+        y1 = y2 = lastY;
+        //Go down and right
+        while (y1 >= 0 && x1 < width && at(x1, y1) == player) {
+            ++x1;
+            --y1;
+        }
+        //Go up and left
+        while (y2 < height && x2 >= 0 && at(x2, y2) == player) {
+            --x2;
+            ++y2;
+        }
+        //Do we have a win here?
+        if (x1 - x2 > toWin) {
+            return player;
+        }
+
+        //                                  X
+        // Do we have a win like this?    X
+        //                              X
+        x1 = x2 = lastX;
+        y1 = y2 = lastY;
+        //Go down and left
+        while (y1 >= 0 && x1 >= 0 && at(x1, y1) == player) {
+            --x1;
+            --y1;
+        }
+        //Go up and right
+        while (y2 < height && x2 < width && at(x2, y2) == player) {
+            ++x2;
+            ++y2;
+        }
+        //Do we have a win here?
+        if (x2 - x1 > toWin) {
+            return player;
+        }
+
+        //No one is the winner
+        return ' ';
+    }
+
+    public static int getScore() {
+        int score = 0;
+        int width = COLS;
+        int height = ROWS;
+
+        //Pieces in the middle score higher than
+        //pieces at the edge
+
+        for (int x = 0; x < width; ++x) {
+            int columnscore = (width / 2) - x;
+            if (columnscore < 0) {
+                columnscore = -columnscore;
+            }
+            columnscore = (width / 2) - columnscore;
+
+            //Count the number of pieces in each column
+            //and score accordingly
+            for (int y = 0; y < height; ++y) {
+                int rowscore = (height / 2) - y;
+                if (rowscore < 0) {
+                    rowscore = -rowscore;
+                }
+                rowscore = (height / 2) - rowscore;
+
+                if (at(x, y) == 'X') {
+                    score += columnscore + rowscore;
+                } else if (at(x, y) == 'O') {
+                    score -= columnscore + rowscore;
+                }
+            }
+        }
+
+        return score;
+    }
+
+    public static char at(int x, int y) {
+        if (board[x][y].equals(Token.player1)) {
+            return 'O';
+        } else if (board[x][y].equals(Token.player2)) {
+            return 'X';
+        } else {
+            return ' ';
+        }
+    }
+
+    /** returns a graphical representation of the board */
+    public static String displayBoard(Token[][] myBoard) {
+        String rowDelimiter = "+";
+        String rowNumbering = " ";
+        for (int col = 0; col < myBoard.length; col++) {
+            rowDelimiter += "---+";
+            rowNumbering += " " + (col + 1) + "  ";
+        }
+        rowDelimiter += "\n";
+
+        String rowStr;
+        String presentation = rowDelimiter;
+        for (int row = myBoard[0].length - 1; row >= 0; row--) {
+            rowStr = "| ";
+            for (int col = 0; col < myBoard.length; col++) {
+                rowStr += myBoard[col][row].toString() + " | ";
+            }
+            presentation += rowStr + "\n" + rowDelimiter;
+        }
+        presentation += rowNumbering;
+        return presentation;
+    }
+
+    public static Token getTokenFromChar(char player) {
+        Token token = Token.empty;
+        if (player == 'O') {
+            token = Token.player1;
+        }
+        if (player == 'X') {
+            token = Token.player2;
+        }
+        return token;
+    }
+
+    public static void move(int x, char player) {
+        int height = ROWS;
+        int width = COLS;
+        //Find the first free place and place a
+        //brick there
+        for (int y = 0; y < height; ++y) {
+            if (at(x, y) == ' ') {
+                board[x][y] = getTokenFromChar(player);
+                lastX = x;
+                lastY = y;
+                return;
+            }
+        }
+    }
+
+    public static boolean canMove(int x) {
+        return board[x][ROWS - 1].equals(Token.empty);
+    }
+
+    public static void undo(int x, char player) {
+        //Find the last non-free place and place a
+        int y = ROWS - 1;
+        while (y >= 0 && at(x, y) == ' ') {
+            --y;
+        }
+        if (at(x, y) == player) {
+            board[x][y] = Token.empty;
+        }
+    }
+
+    /** main method, starts the program */
+    public static void main(String args[]) {
+        VierGewinnt game = new VierGewinnt();
+        game.play();
+    }
 }
