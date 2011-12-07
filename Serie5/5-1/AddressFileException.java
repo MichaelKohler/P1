a7fe422631831bf1b51ab78260765824b8ffdea8
Michael Kohler <michaelkohler@live.com>
2011-12-07 17:23:20 +0100

    Matrikelnummer correction

--- Serie5/5-1/AddressFileException.java
+++ Serie5/5-1/AddressFileException.java
@@ -2,7 +2,7 @@
  * P1 Aufgabe 5-1 / AddressFileException.java
  * Authors:
  *  - Michael Kohler - 11-108-289
- *  - Lukas Diener - 11-123-231
+ *  - Lukas Diener - 11-123-213
  */
 
 public class AddressFileException extends Exception {
